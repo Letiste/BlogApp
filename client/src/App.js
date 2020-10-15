7c1b499d7e729359bcf15f38b3045540a6e0f98a
Léo Salé <leo.sale72@gmail.com>
2020-10-15 17:17:45 +0200

    Setup react router (#2)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+import Homepage from "./Component/Homepage/Homepage"
+import CreateBlog from "./Component/CreateBlog/CreateBlog"
 
 function App() {
-  return <h1>Blog App</h1>;
+  return (
+    <Router>
+      <Route path="/" exact component={Homepage} />
+      
+      <Route path="/create" exact component={CreateBlog} />
+
+    </Router>
+  )
 }
 
 export default App;
