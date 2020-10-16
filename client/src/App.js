6c0c5da04afc42beac8cc6712d3717cce46d4930
Léo Salé <leo.sale72@gmail.com>
2020-10-16 12:35:42 +0200

    add header

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Homepage from "./Component/Homepage/Homepage";
 import CreateBlog from "./Component/CreateBlog/CreateBlog";
 
+import "./App.css"
+
 function App() {
   return (
+    
     <Router>
+
+      <h1 className="header"><a href="/">BlogApp</a></h1>
       <Route path="/" exact component={Homepage} />
 
       <Route path="/create" exact component={CreateBlog} />
