24ebd7d566f05ef073b50c07bda638f1bdb3a784
Léo Salé <leo.sale72@gmail.com>
2020-10-17 12:37:51 +0200

    convert rawdraft to markdown

--- client/src/Component/CreatePost/CreatePost.js
+++ client/src/Component/CreatePost/CreatePost.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Editor } from 'react-draft-wysiwyg';
+import { convertToRaw } from 'draft-js';
+import draftToMarkdown from 'draftjs-to-markdown';
 import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 import './CreatePost.css';
 
+function onEditorStateChange(editorState, setValue) {
+  const rawContentState = convertToRaw(editorState.getCurrentContent());
+  setValue(draftToMarkdown(rawContentState));
+}
+
 export default function CreatePost() {
+  const [markdown, setMarkdown] = useState();
+
+  function handlePublish() {
+    console.log(markdown);
+  }
+
   return (
     <div className="createPost">
       <h2>Create a Post</h2>
       <div className="form">
         <div className="formTop">
           <div className="detailsPost">
-            <label className="label" for="title">
+            <label className="label" htmlFor="title">
               Title :
             </label>
             <input
@@ -19,17 +32,25 @@ export default function CreatePost() {
               id="title"
               placeholder="My super Post"
             />
-            <label className="label" for="autor">
+            <label className="label" htmlFor="autor">
               Autor :
             </label>
             <input className="inputForm" id="autor" placeholder="John Doe" />
           </div>
-          <input className="publishButton" type="submit" value="Publish" />
+          <input
+            className="publishButton"
+            type="submit"
+            value="Publish"
+            onClick={handlePublish}
+          />
         </div>
         <Editor
           wrapperClassName="wrapper-class"
           editorClassName="editor-class"
           toolbarClassName="toolbar-class"
+          onEditorStateChange={(editorState) =>
+            onEditorStateChange(editorState, setMarkdown)
+          }
         />
       </div>
     </div>
