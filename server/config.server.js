d865a6ec6e94171e0a965ee57617d4396b491156
Léo Salé <leo.sale72@gmail.com>
2020-10-17 17:05:13 +0200

    fix cors error

--- server/config.server.js
+++ server/config.server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
 const app = express();
+
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://172.30.136.227:3000'],
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
