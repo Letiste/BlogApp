5e3d4eb754b7c5b1e53aaa7d7f3adacba9e275bb
Léo Salé <leo.sale72@gmail.com>
2020-10-17 11:54:46 +0200

    style create post page

--- client/src/Component/CreatePost/CreatePost.js
+++ client/src/Component/CreatePost/CreatePost.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React from 'react';
+import { Editor } from 'react-draft-wysiwyg';
+import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-export default function CreateBlog() {
-  return <h1>Create blog</h1>;
+import './CreatePost.css';
+
+export default function CreatePost() {
+  return (
+    <div className="createPost">
+      <h2>Create a Post</h2>
+      <div className="form">
+        <div className="formTop">
+          <div className="detailsPost">
+            <label className="label" for="title">
+              Title :
+            </label>
+            <input
+              className="inputForm"
+              id="title"
+              placeholder="My super Post"
+            />
+            <label className="label" for="autor">
+              Autor :
+            </label>
+            <input className="inputForm" id="autor" placeholder="John Doe" />
+          </div>
+          <input className="publishButton" type="submit" value="Publish" />
+        </div>
+        <Editor
+          wrapperClassName="wrapper-class"
+          editorClassName="editor-class"
+          toolbarClassName="toolbar-class"
+        />
+      </div>
+    </div>
+  );
 }
