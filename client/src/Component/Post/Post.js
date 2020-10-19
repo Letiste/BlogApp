07a60d1038755323f9ebb8071ffe95380230b50e
Léo Salé <leo.sale72@gmail.com>
2020-10-19 19:16:58 +0200

    style post

--- client/src/Component/Post/Post.js
+++ client/src/Component/Post/Post.js
@@ -5,30 +5,43 @@ import { convertFromRaw, EditorState } from 'draft-js';
 
 import PostService from '../../services/PostService';
 
+import './Post.css';
+
 export default function Post() {
-  const [post, setPost] = useState({});
+  const [post, setPost] = useState();
 
   const { id } = useParams();
 
   useEffect(() => {
     PostService.getOne(id)
-      .then((res) => setPost(res.data))
+      .then((res) => {
+        setPost(res.data);
+        console.log(res.data.updatedAt);
+      })
       .catch(console.log);
-  });
+  }, [id]);
 
   return (
-    <div>
-      <p>
-        {post.title}, {post.author}
-      </p>
-      <Editor
-        readOnly
-        toolbarHidden
-        editorState={
-          post.content &&
-          EditorState.createWithContent(convertFromRaw(post.content))
-        }
-      />
-    </div>
+    <>
+      {post && (
+        <div className="post">
+          <h2 className="postTitle">{post.title}</h2>
+          <p className="postInfo">
+            <span className="postAuthor">{post.author}</span>
+            <span className="postDate">{post.updatedAt.split('T')[0]}</span>
+          </p>
+          <div className="postSeparator" />
+          <Editor
+            className="postContent"
+            readOnly
+            toolbarHidden
+            editorState={
+              post.content &&
+              EditorState.createWithContent(convertFromRaw(post.content))
+            }
+          />
+        </div>
+      )}
+    </>
   );
 }
